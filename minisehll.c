3e2b3b2d103c1ddd187ba0935c13494165cb192c
Abdelhamid Kaouan <akaouan@e2r11p2.1337.ma>
2022-05-07 12:23:48 +0000

    set up

--- minisehll.c
+++ minisehll.c
@@ -2,12 +2,5 @@
 
 int main(int ac, char **av)
 {
-    t_data  *data;
 
-    data = NULL;
-    if (!init_data(&data, av, ac))
-        error("Fail to allocate the data\n");
-    for(int i = 0; i < ac; i++){
-        printf(RED "%s\n" RESET, data->terminal[i]);
-    }
 }
