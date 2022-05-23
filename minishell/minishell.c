91c361d98ce6a82d6d9c1bc7f1427a515702fcb2
Abdelaziz Elhayyany <100360875+elhayyany@users.noreply.github.com>
2022-05-23 18:50:24 +0100

    12th edition

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -6,47 +6,54 @@
 /*   By: ael-hayy <ael-hayy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 09:50:09 by ael-hayy          #+#    #+#             */
-/*   Updated: 2022/05/22 17:44:17 by ael-hayy         ###   ########.fr       */
+/*   Updated: 2022/05/23 18:45:33 by ael-hayy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "executor/minishell.h"
 
-int main(int ac, char **sv, char **env)
-{
-	char     *line;
-	t_prior  *script;
-	char		**operators;
+// int main(int ac, char **sv, char **env)
+// {
+// 	char     *line;
+// 	t_prior  *script;
+// 	char		**operators;
 
-	line = 0;
-	while (1)
-	{
-		line = readline(">$ ");
-		if (!line  || line[0] == '\0')
-		{
-			if (line && line[0] == '\0')
-			{
-				free(line);
-				continue ;
-			}
-			write(1, "\n", 1);
-			continue ;
-		}
-		if (!all_space(line))
-		{
-			free(line);
-			continue ;
-		}
-		// if (pre_check_line(line))
-		// {
-		//    free(line);
-		//    continue ;
-		// }
-		script = m_shell_parser(line, &operators);
-		tree_parser(script, char **env);
-		free_tree(script);
-		free(script);
-		free (line);
-   }
+// 	line = 0;
+// 	while (1)
+// 	{
+// 		line = readline(">$ ");
+// 		if (!line  || line[0] == '\0')
+// 		{
+// 			if (line && line[0] == '\0')
+// 			{
+// 				free(line);
+// 				continue ;
+// 			}
+// 			write(1, "\n", 1);
+// 			continue ;
+// 		}
+// 		if (!all_space(line))
+// 		{
+// 			free(line);
+// 			continue ;
+// 		}
+// 		// if (pre_check_line(line))
+// 		// {
+// 		//    free(line);
+// 		//    continue ;
+// 		// }
+// 		script = m_shell_parser(line, &operators);
+// 		tree_parser(script, char **env);
+// 		free_tree(script);
+// 		free(script);
+// 		free (line);
+//    }
+// }
+
+
+
+int main(int ac, char **av, char **env)
+{
+
 }
